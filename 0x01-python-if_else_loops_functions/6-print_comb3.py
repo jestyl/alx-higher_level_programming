417bc726378187eb207503be11cfd526a283b3e6
Jestyl <jestylarku@gmail.com>
2024-01-03 18:43:57 -0800

    if and loops

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(9):
+for i in range(10):
     for j in range(i + 1, 10):
-        if i * 10 + j < 89:
-            print("{:d}{:d}".format(i, j), end=", ")
-            print("{:d}".format(89))
+        print(f"{i}{j}", end=", " if j < 9 or i < 8 else "\n")
