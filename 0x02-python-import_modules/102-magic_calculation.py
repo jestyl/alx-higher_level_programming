96953b10b0fb8ec95063725468d75a76fb6d4336
Jestyl <jestylarku@gmail.com>
2024-01-04 19:06:22 -0800

    Magic

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
-
     from magic_calculation_102 import add, sub
-
     if a < b:
         rsl = add(a, b)
-
         for i in range(4, 6):
             rsl = add(rsl, i)
-
         return (rsl)
-
     else:
         return(sub(a, b))
