197266e32db51c5c77e5600a6eb600bb9c8d3b81
Jestyl <jestylarku@gmail.com>
2024-01-23 21:49:02 -0800

    Python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Module of a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes new Square.
+
+        Args:
+            size: The size of new square.
+        """
+        self.__size = size
