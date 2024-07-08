1fdb54747660b613be9d44a436eb2426f2112a9f
Jestyl <jestylarku@gmail.com>
2024-07-08 10:21:07 -0700

    fix 5.6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,12 +7,15 @@ def find_peak(list_of_integers):
     def find_peak_util(low, high):
         mid = (low + high) // 2
 
+        # Check if mid is a peak
         if (mid == 0 or list_of_integers[mid - 1] <= list_of_integers[mid]) and (mid == len(list_of_integers) - 1 or list_of_integers[mid + 1] <= list_of_integers[mid]):
             return list_of_integers[mid]
 
+        # If the left neighbor is greater, the peak is in the left half
         elif mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
             return find_peak_util(low, mid - 1)
 
+        # If the right neighbor is greater, the peak is in the right half
         else:
             return find_peak_util(mid + 1, high)
 
